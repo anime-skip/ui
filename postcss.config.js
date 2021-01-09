fa44b82d71de831df24e1d3fd5cccdafbb5f9dbf
Aaron Klinker <aaronklinker1@gmail.com>
2021-01-09 17:22:33 -0600

    feat: Setup library builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // tailwindcss: {},
-    // autoprefixer: {},
-  ],
+  plugins: [require('tailwindcss'), require('autoprefixer')],
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,46 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import vue from 'rollup-plugin-vue';
+import scss from 'rollup-plugin-scss';
+import copy from 'rollup-plugin-copy';
+import del from 'rollup-plugin-delete';
+import execute from 'rollup-plugin-execute';
 
 import packageJson from './package.json';
 
 export default {
-  input: 'src/index.ts',
+  input: 'src/lib/index.ts',
   output: [
     {
       format: 'cjs',
       file: packageJson.main,
       sourcemap: true,
+      exports: 'named',
     },
     {
       format: 'esm',
       file: packageJson.module,
       sourcemap: true,
+      exports: 'named',
     },
   ],
-  plugins: [peerDepsExternal(), resolve(), commonjs(), typescript(), vue()],
+  plugins: [
+    del({ targets: 'lib/*' }),
+    scss(),
+    typescript({ tsconfig: 'tsconfig.build.json' }),
+    vue(),
+    resolve(),
+    commonjs(),
+    execute([
+      // Generate latest tailwind scss
+      'yarn build:tailwind-scss',
+      // Duplicate main declaration file so webpack is happy
+      'cp lib/index.d.ts lib/index.esm.d.ts',
+    ]),
+    copy({
+      targets: [{ src: 'src/lib/scss', dest: 'lib' }],
+      targets: [{ src: 'src/lib/tailwind.preset.js', dest: 'lib', rename: 'tailwind-preset.js' }],
+    }),
+  ],
 };
