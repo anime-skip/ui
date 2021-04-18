4d82c0f31aea03fc5e207d35368219d0b54dc757
Aaron Klinker <aaronklinker1@gmail.com>
2021-04-18 14:51:37 -0500

    fix: Put @imports at top of style.css
    This removes a warning that shows when using Vite

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,19 +15,33 @@ cp src/lib/tailwind.preset.js lib/
 cp src/lib/scss/theme.scss lib/
 
 # Add tailwind setup and utils to style.css
-echo -e "\n" >> lib/style.css
-cat src/lib/scss/tailwind.css >> lib/style.css
-
-echo -e "\n" >> lib/style.css
-cat src/lib/scss/fonts.css >> lib/style.css
-
-echo -e "\n" >> lib/style.css
-cat src/lib/scss/defaults.css >> lib/style.css
-
-echo -e "\n" >> lib/style.css
-yarn sass src/lib/scss/utilities.scss lib/.temp.css 
-cat lib/.temp.css >> lib/style.css
-rm -rf lib/.temp.css*
+echo -e "\n/* compiled tailwind styles for components */\n" | cat - lib/style.css > lib/.temp.css && mv lib/.temp.css lib/style.css
+echo "" >> lib/style.css
+function appendCssToStyles {
+    # $1: The path to the .css file who's content will be appended
+    echo "Appending $1"
+    echo -e "\n/* $1 */\n" >> lib/style.css
+    cat "$1" >> lib/style.css
+}
+function appendScssToStyles {
+    # $1: The path to the .scss file who's content will be compiled and appended
+    echo "Appending $1"
+    echo -e "\n/* $1 */\n" >> lib/style.css
+    yarn -s sass "$1" lib/.temp.css 
+    cat lib/.temp.css >> lib/style.css
+    rm -rf lib/.temp.css*
+}
+function prependCssToStyles {
+    # $1: The path to the .css file who's content will be prepended
+    echo "Prepending $1"
+    echo -e  "/* $1 */\n" | cat - "$1" | cat - lib/style.css > lib/.temp.css
+    mv lib/.temp.css lib/style.css
+}
+prependCssToStyles "src/lib/scss/imports.css"
+appendCssToStyles  "src/lib/scss/tailwind.css"
+appendCssToStyles  "src/lib/scss/fonts.css"
+appendCssToStyles  "src/lib/scss/defaults.css"
+appendScssToStyles "src/lib/scss/utilities.scss"
 
 # Copy package.json to build from the source
 cp package.json lib/
