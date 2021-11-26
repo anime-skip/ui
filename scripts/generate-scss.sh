752f91cff896751dd0b4249f2f3a6c552cc219fe
Aaron Klinker <aaronklinker1@gmail.com>
2021-11-26 09:47:28 -0600

    fix: remove scss lint errors from generated variables

--- scripts/generate-scss.sh
+++ scripts/generate-scss.sh
@@ -7,3 +7,13 @@ yarn -s tailwindcss-export-config \
     --destination=src/lib/styles/variables-theme \
     --format=scss \
     --flat=true
+
+# Fix lint
+INPUT_FILE="src/lib/styles/variables-theme.scss"
+TEMP_OUTPUT="src/lib/styles/variables-theme.scss-temp"
+PATTERN="1 / -1"
+while read -r line
+do
+  [[ ! $line =~ "$PATTERN" ]] && echo "$line"
+done <"$INPUT_FILE" > "$TEMP_OUTPUT"
+mv "$TEMP_OUTPUT" "$INPUT_FILE"
