a8f668ca302acc92dae777ca7f4df64fcfbaf3c7
Aaron Klinker <aaronklinker1@gmail.com>
2021-12-27 12:09:55 -0600

    testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    '@storybook/preset-scss',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     {
